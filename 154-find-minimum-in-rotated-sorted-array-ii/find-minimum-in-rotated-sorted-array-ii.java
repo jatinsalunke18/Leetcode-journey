117912465541531fa18a45c514e612678a54cf3d
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-05 12:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (88.67%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        int min = Integer.MAX_VALUE;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[low]<=nums[high]) min = Math.min(nums[low],min);
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){
+                min = Math.min(nums[mid],min);
+                low = mid+1;
+            }
+            else{
+                min = Math.min(nums[mid],min);
+                high = mid-1;
+            }
+        }
+        return min;
+    }
+}
