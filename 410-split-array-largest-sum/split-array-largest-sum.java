71b289b08500bc65cee4cdb658a9bef40d55089d
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-12 20:14:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (69.17%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,32 +1,31 @@
 class Solution {
-    int partitions(int nums[],int mid){
+    int check(int[] nums,int mid){
         int cnt = 1;
         int sum = 0;
-        for(int num : nums){
-            if(num+sum>mid){
+        for(int num:nums){
+            if(num+sum > mid){
                 cnt++;
                 sum = num;
             }
-            else sum+=num;
+            else sum += num;
         }
         return cnt;
     }
-    
     public int splitArray(int[] nums, int k) {
-        int low = nums[0];
+        int low = Integer.MIN_VALUE;
         int high = 0;
-        int ans = 0;
         for(int num:nums){
-            if(num>low) low=num;
+            if(num>low) low = num;
             high += num;
-        } 
+        }
         while(low<=high){
-            int mid = (low+high)/2;
-            if(partitions(nums,mid)<=k){
+            int mid = low+(high-low)/2;
+            if(check(nums,mid)<=k){
                 high = mid-1;
             }
             else low = mid+1;
         }
         return low;
     }
+
 }
