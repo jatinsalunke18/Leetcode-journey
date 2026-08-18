6546b94e62d90b74502eeb27f5a750e8874d3cb9
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-18 08:24:06 +0530

    Time: 2 ms (72.63%) | Memory: 44.2 MB (98.95%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,16 +1,32 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
         int ans = -1;
-        for(int i=0;i<=nums.length-k;i++){
-            for(int j=i;j<k+i;j++){
-                map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+        if(k==nums.length){
+            for(int i=0;i<nums.length;i++) ans = Math.max(nums[i],ans);
+            return ans;
+        }
+        if(k==1){
+            HashMap<Integer,Integer> map = new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             }
+            for(int i=0;i<nums.length;i++){
+                if(map.get(nums[i])==1) ans = Math.max(ans,nums[i]);
+            }
+            return ans;
         }
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])==1) ans = Math.max(ans,nums[i]);
-            if(nums.length==k) ans = Math.max(ans,nums[i]);
+        int first = nums[0];
+        int last = nums[nums.length-1];
+        if(first==last) return -1;
+        int freq1 = 1;
+        int freq2 = 1;
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i]==first) freq1++;
+            if(nums[i]==last) freq2++;
         }
-        return ans;
+        if(freq1==1 && freq2 == 1) return Math.max(first,last); 
+        if(freq1!=1 && freq2==1) return last;
+        if(freq1==1 && freq2!=1) return first;  
+        return -1; 
     }
 }
