a61aee8471d35c01d44839ceff2382567b4eced4
jatinsalunke18 <jatinsalunke1805@gmail.com>
2026-08-22 11:26:06 +0530

    Time: 6 ms (14.51%) | Memory: 46.9 MB (97.83%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumLength(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        if(s.charAt(left)!=s.charAt(right)) return (right-left)+1;
+        while(left<right){
+            while(left<right && s.charAt(left)==s.charAt(left+1)) left++;
+            while(left<right && s.charAt(right)==s.charAt(right-1)) right--;
+            left++;
+            right--; 
+            if(left<=right && s.charAt(left)!=s.charAt(right)) return (right-left)+1;
+        }
+        return Math.max(0,(right-left)+1);
+    }
+}
